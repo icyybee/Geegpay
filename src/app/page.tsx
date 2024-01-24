6d7dfa7fe8dca513a2b62d4e7cc3fd60ad51e92e
icyybee <ushiksmandi@gmail.com>
2024-01-24 22:08:42 +0100

    dynamic loading for next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@
 import Loader from "@/components/Loader";
 import { useEffect, useState } from "react";
 import DashboardLayout from "../layout/DashboardLayout";
-import Dashboard from "./dashboard/page";
 import { DarkModeProvider } from "@/context/DarkModeContext";
+import dynamic from "next/dynamic";
+
+const Dashboard = dynamic(() => import('./dashboard/page'), { ssr: false });
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
