d7d94f4d127714f437e09de89d76cc46619667a3
icyybee <ushiksmandi@gmail.com>
2024-01-25 10:27:52 +0100

    manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 const jakarta_font = Plus_Jakarta_Sans({ style: 'normal', weight: '400', subsets: ["latin"]}) 
-
 export const metadata: Metadata = {
   title: "Geegpay",
   description: "Geegpay Frontend Hackathon Challenge created using NextJs and Typescript",
@@ -15,6 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/Vector.svg" /> 
       <body className={jakarta_font.className}>{children}</body>
     </html>
   );
