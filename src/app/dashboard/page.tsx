e5edf79a49df6c6ea17e45a771cf8ea884f82537
Beatrice Egumandi <106768860+icyybee@users.noreply.github.com>
2024-10-28 16:16:47 +0100

    style: larger screens

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,20 @@
-import Analysis from '@/components/analysis/Analysis'
-import LastOrders from '@/components/lastorders/LastOrders'
-import SalesTrend from '@/components/salestrend/SalesTrend'
-import TopPlatform from '@/components/topplatform/TopPlatform'
-import React from 'react'
+import Analysis from "@/components/analysis/Analysis";
+import LastOrders from "@/components/lastorders/LastOrders";
+import SalesTrend from "@/components/salestrend/SalesTrend";
+import TopPlatform from "@/components/topplatform/TopPlatform";
+import React from "react";
 
 export default function Dashboard() {
-    return (
-        <div className='mt-5 px-5 w-full flex flex-col gap-5'>
-            <div className='desktop:flex-row flex-col flex gap-5 w-full items-center'>
-                <SalesTrend />
-                <Analysis />
-            </div>
-            <div className='desktop:flex-row flex-col flex w-full gap-5'>
-                <LastOrders />
-                <TopPlatform />
-            </div>
-        </div>
-    )
+  return (
+    <div className="mt-5 px-5 max-w-[1400px] mx-auto w-full flex flex-col gap-5">
+      <div className="desktop:flex-row flex-col flex gap-5 w-full items-center">
+        <SalesTrend />
+        <Analysis />
+      </div>
+      <div className="desktop:flex-row flex-col flex w-full gap-5">
+        <LastOrders />
+        <TopPlatform />
+      </div>
+    </div>
+  );
 }
--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,15 +1,19 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
-import { useDarkMode } from '@/context/DarkModeContext'
-import Menu from './Menu'
+import React, { useState } from "react";
+import { useDarkMode } from "@/context/DarkModeContext";
+import Menu from "./Menu";
 
 export default function Aside() {
-    const { isDarkMode } = useDarkMode()
-    const [openDrawer, setOpenDrawer] = useState<boolean>(false)
-    return (
-        <aside className={`py-5 w-20 overflow-y-hidden ${isDarkMode ? 'bg-darkcard' : 'bg-sidebar'} mids:fixed mids:block hidden`}>
-            <Menu setOpenDrawer={setOpenDrawer}/>
-        </aside>
-    )
+  const { isDarkMode } = useDarkMode();
+  const [openDrawer, setOpenDrawer] = useState<boolean>(false);
+  return (
+    <aside
+      className={`py-5 w-20 overflow-y-hidden h-full ${
+        isDarkMode ? "bg-darkcard" : "bg-sidebar"
+      } mids:fixed mids:block hidden`}
+    >
+      <Menu setOpenDrawer={setOpenDrawer} />
+    </aside>
+  );
 }
